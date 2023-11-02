65cab22583d16c8251484e22faf308868d61c07f
Dalton Burkhart <137221425+dburkhart07@users.noreply.github.com>
2023-11-02 01:19:15 -0400

    Add files via upload

---
+++ phonenumber_location_tracker.py
@@ -0,0 +1,25 @@
+import phonenumbers
+from phonenumbers import carrier,geocoder,timezone
+
+Key = "6d6f969fd9024ac8afde957f0c86a5ba"
+
+number="+19999999999"
+parsed_num = phonenumbers.parse(number)
+number_location = geocoder.description_for_number(parsed_num,"en")
+print(number_location)
+
+
+#service_provider = phonenumbers.parse(number)
+#print(carrier.name_for_number(service_provider, "en"))
+
+#from opencage.geocoder import OpenCageGeocode
+#geocoder = OpenCageGeocode(Key)
+
+#query = str(number_location)
+#results = geocoder.geocode(query)
+
+#lat = results[0]['geometry']['lat']
+#lng = results[0]['geometry']['lng']
+#print(lat,lng)
+
+
